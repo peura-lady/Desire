9de7a987a34280b69d7785d6e88747967e30ea9f
peura-lady <anna.sonninen94@gmail.com>
2021-05-21 23:22:47 +0300

    added rightside menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
-
-})
+    $('.header__btn').on('click', function () {
+        $('.rightside-menu').removeClass('rightside-menu--close');
+    });
+    $('.rightside-menu__close').on('click', function () {
+        $('.rightside-menu').addClass('rightside-menu--close');
+    });
+});
