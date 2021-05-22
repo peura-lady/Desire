443cc372b87cc1ad4476a483ed1171c41f00522d
peura-lady <anna.sonninen94@gmail.com>
2021-05-22 20:51:45 +0300

    added slick-slider to the main html page

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,12 @@ $(function () {
     $('.rightside-menu__close').on('click', function () {
         $('.rightside-menu').addClass('rightside-menu--close');
     });
+
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        // autoplaySpeed: 2000,
+    });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ function images() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
+    'node_modules/slick-carousel/slick/slick.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
