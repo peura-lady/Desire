eceddc80e52ee2616a1757d411345ba02da55cf4
peura-lady <anna.sonninen94@gmail.com>
2021-06-05 23:26:16 +0300

    started about page. Added fancybox feature, breadcrumps, about and inspiration sections

--- gulpfile.js
+++ gulpfile.js
@@ -43,6 +43,7 @@ function scripts() {
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/mixitup/dist/mixitup.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
